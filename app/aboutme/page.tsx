f815e72e45272d0adfbdba8f908c78304f65892a
Can A. <58741540+canameti@users.noreply.github.com>
2025-01-27 11:59:39 +0100

    Update page.tsx

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -23,15 +23,13 @@ export default function Example() {
                   {s.label}
                 </span>
 				<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                  I’m a 23-year-old System Engineer based in Germany.
-                  <br /> I grew up in the dynamic city of Berlin, where I completed my education,
-                  and my background reflects both German and Turkish heritage.
-                  <br /> Now, I work for a company in Bavaria, specializing in Managed Cloud Hosting, IT Operations, and Consulting.
+					I’m a 23-year-old System Engineer based in Germany.
+                  <br /> I grew up in the vibrant city of Berlin, where I completed my education, and my background reflects both German and Turkish heritage.
+                  <br /> Currently, I work for a company in Bavaria, specializing in Managed Cloud Hosting, IT Operations, and Consulting. In my free time, I pursue freelance web development, creating websites using Next.js and TailwindCSS. I’m also skilled in SvelteKit and Vue.js.
                   <br />
-                  While I occasionally code in C#, TypeScript, JavaScript, and Go, these days I spend more of my free time outside,
-                  or enjoying my favorite sports — Formula 1 and ice hockey.
+				  When I’m not coding, you’ll probably find me outdoors or enjoying my favorite sports — Formula 1 and ice hockey.
 				  <br />
-                  Feel free to connect if you'd like to discuss tech, sports, or just life!
+                  Feel free to connect if you'd like to discuss web development, sports, or just life in general!
                 </span>
 							</div>
 						</Card>
